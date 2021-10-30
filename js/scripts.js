0c277e081a43b2f8a1171374dcc957136f152332
Dreyn <dreenusg2403@gmail.com>
2021-10-30 15:08:55 -0300

    new menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-$(document).ready(function() {
-    $(".hamburguer").click(function(){
-      $(this).toggleClass('active')
-      $(".menu").toggleClass('active')
-    })
-  })
+$(function(){
+        $(".button-collapse").sideNav();
+    });
