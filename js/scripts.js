eae99b0c02e6c83efeabd1317523f50bb7dc21cd
Dreyn <dreenusg2403@gmail.com>
2021-11-14 17:11:18 -0300

    mucha coisa

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
 $(function(){
         $(".button-collapse").sideNav();
     });
+
+// var bghtml = window.document.querySelector('div.conteudo')
+// bghtml.addEventListener('mouseenter', entrar)
+// bghtml.addEventListener('mouseout', sair)
+
+// function entrar () {
+//     bghtml.style.transition = '0.3s'
+//     bghtml.style.background = 'url(./img/backgroundfade-conteudo.jpg)'
+//     bghtml.style.backgroundPosition = 'center'
+//     bghtml.style.backgroundPositionY = '-300px'
+//     bghtml.style.backgroundRepeat = 'no-repeat'
+//     bghtml.style.backgroundSize = 'cover'
+
+// }
+
+// function sair () {
+//     bghtml.style.background = 'url(./img/background-conteudo.jpg)'
+//     bghtml.style.backgroundPosition = 'center'
+//     bghtml.style.backgroundPositionY = '-300px'
+//     bghtml.style.backgroundRepeat = 'no-repeat'
+//     bghtml.style.backgroundSize = 'cover'
+// }
